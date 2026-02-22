80c0824f12e7eea6326e0c3fca4d8ba684ecee80
Wazza <info@thefirm.club>
2026-02-22 19:27:47 +0000

    Add +44 perms to Civ Command

--- Commands/AddRoleCommand.cs
+++ Commands/AddRoleCommand.cs
@@ -70,6 +70,7 @@ public class AddRoleCommand : ISlashCommand
         1469355919510077470, // GSC
         1469654367593566349, // Lost MC
         1471129098649735311, // Zero Tolerance
+        1475211012323020921, // +44
     };
 
     public async Task ExecuteAsync(SocketSlashCommand command)
--- Commands/RemoveRoleCommand.cs
+++ Commands/RemoveRoleCommand.cs
@@ -71,6 +71,7 @@ public class RemoveRoleCommand : ISlashCommand
         1469355919510077470, // GSC
         1469654367593566349, // Lost MC
         1471129098649735311, // Zero Tolerance
+        1475211012323020921, // +44
     };
 
     public async Task ExecuteAsync(SocketSlashCommand command)
