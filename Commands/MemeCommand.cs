a183f5a81a93125d840ef860e648ba45afd3db49
smallp13 <alcw8888@gmail.com>
2026-03-28 10:52:24 +0000

    Update MemeCommand.cs

--- Commands/MemeCommand.cs
+++ Commands/MemeCommand.cs
@@ -61,11 +61,11 @@ public class MemeCommand : ISlashCommand
 
                 // TOP TEXT
                 DrawText(graphics, topText.ToUpper(), font,
-                    new Drawing.RectangleF(0, 10, image.Width, image.Height / 5), sf);
+                    new Drawing.RectangleF(0, 0, image.Width, image.Height / 4));
 
                 // BOTTOM TEXT
                 DrawText(graphics, bottomText.ToUpper(), font,
-                    new Drawing.RectangleF(0, image.Height - (image.Height / 5) - 10, image.Width, image.Height / 5), sf);
+                    new Drawing.RectangleF(0, image.Height - (image.Height / 4), image.Width, image.Height / 4));
 
                 var path = $"meme_{Guid.NewGuid()}.png";
                 image.Save(path, DrawingImaging.ImageFormat.Png);
@@ -81,19 +81,32 @@ public class MemeCommand : ISlashCommand
         }
     }
 
-    private void DrawText(Drawing.Graphics g, string text, Drawing.Font font, Drawing.RectangleF rect, Drawing.StringFormat sf)
+    private void DrawText(Drawing.Graphics g, string text, Drawing.Font font, Drawing.RectangleF rect)
     {
-        // Outline
+        var format = new Drawing.StringFormat
+        {
+            Alignment = Drawing.StringAlignment.Center,
+            LineAlignment = Drawing.StringAlignment.Center,
+            FormatFlags = Drawing.StringFormatFlags.LineLimit
+        };
+        var size = g.MeasureString(text, font, rect.Size);
+
+        while ((size.Width > rect.Width || size.Height > rect.Height) && font.Size > 10)
+        {
+            font = new Drawing.Font(font.FontFamily, font.Size - 2, Drawing.FontStyle.Bold);
+            size = g.MeasureString(text, font, rect.Size);
+        }
+
         for (int x = -2; x <= 2; x++)
         {
             for (int y = -2; y <= 2; y++)
             {
                 g.DrawString(text, font, Drawing.Brushes.Black,
-                    new Drawing.RectangleF(rect.X + x, rect.Y + y, rect.Width, rect.Height), sf);
+                    new Drawing.RectangleF(rect.X + x, rect.Y + y, rect.Width, rect.Height),
+                    format);
             }
         }
 
-        // Main text
-        g.DrawString(text, font, Drawing.Brushes.White, rect, sf);
+        g.DrawString(text, font, Drawing.Brushes.White, rect, format);
     }
 }
