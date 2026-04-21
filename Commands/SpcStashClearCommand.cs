b3af2b7e82cb5f42f26428c30218788891fbf5fb
smallp13 <alcw8888@gmail.com>
2026-04-21 11:53:48 +0100

    Update SpcStashClearCommand.cs

--- Commands/SpcStashClearCommand.cs
+++ Commands/SpcStashClearCommand.cs
@@ -59,7 +59,7 @@ public class SpcStashClearCommand : ISlashCommand
             int affected = await cmd.ExecuteNonQueryAsync();
 
             var embed = new EmbedBuilder()
-                .WithTitle("🧹 Superintendent+ Evidence Locker Cleared")
+                .WithTitle("🧹 Superintendent+ Evidence Locker")
                 .WithColor(new Color(59, 130, 246))
                 .WithTimestamp(DateTimeOffset.UtcNow)
                 .AddField("Affected Rows", affected, true)
