6805d816190b3d0f2dda14d891a00ca739cdb67d
Wazza <info@thefirm.club>
2026-09-06 21:26:57 +0100

    DeleteAll

--- ISlashCommand.cs
+++
@@ -1,9 +0,0 @@
-using Discord.WebSocket;
-using System.Threading.Tasks;
-
-public interface ISlashCommand
-{
-    string Name { get; }
-    string Description { get; }
-    Task ExecuteAsync(SocketSlashCommand command);
-}
